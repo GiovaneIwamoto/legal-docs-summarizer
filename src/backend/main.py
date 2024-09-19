5835693c9755b1012ce621f174030bfaca7c9b32
Giovane Iwamoto <81972850+GiovaneIwamoto@users.noreply.github.com>
2024-09-19 18:10:50 -0400

    feat: main summarized folder

--- src/backend/main.py
+++ src/backend/main.py
@@ -5,22 +5,22 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from pathlib import Path
 
 from services.ocr_service import process_dataset
+from services.process_summary_service import process_summary
 from utils.extract_zip_file import extract_zip_file
 from utils.save_upload_file import save_upload_file
 
-app = FastAPI()
-
 UPLOAD_FOLDER = "uploads"
 
-@app.post("/upload_file/")
-async def upload_file(file: UploadFile = File(...)):
+app = FastAPI()
+
+@app.post("/generate_summary/")
+async def generate_summary(file: UploadFile = File(...)):
     if not file.filename.endswith(".zip"):
         raise HTTPException(status_code=400, detail="O arquivo deve ser no formato ZIP.")
 
     # Definir o caminho e criar a pasta uploads
     zip_path = os.path.join(UPLOAD_FOLDER, file.filename)
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    print("\nCaminho do arquivo ZIP:", zip_path)
 
     # Salvar o arquivo ZIP
     save_upload_file(file, zip_path)
@@ -41,5 +41,13 @@ async def upload_file(file: UploadFile = File(...)):
     
     # Processar a pasta extraída com o OCR
     process_dataset(extract_folder)
+
+    # Definido paths para as pastas de texto formatado e resumido
+    formatted_folder = os.path.join(extract_folder, "Textos-Formatados")
+    summarized_folder = os.path.join(extract_folder, "Textos-Resumidos")
+    os.makedirs(summarized_folder, exist_ok=True)
+
+    # Processar os textos formatados e gerar os resumos
+    process_summary(formatted_folder, summarized_folder)
 
     return {"detail": "Upload e processamento concluídos com sucesso!"}
