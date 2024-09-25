81ad6f605bf1ecbf5b4a8d543dfc64461303b21f
Giovane Iwamoto <81972850+GiovaneIwamoto@users.noreply.github.com>
2024-09-25 12:31:10 -0400

    fix: js script frontend

--- src/backend/main.py
+++ src/backend/main.py
@@ -25,24 +25,36 @@ async def generate_summary(file: UploadFile = File(...)):
         raise HTTPException(status_code=400, detail="O arquivo deve ser no formato ZIP.")
 
 
-    # # Salvar e extrair o ZIP
+    # Salvar e extrair o ZIP
     extract_folder = save_extract_zip_file(file, UPLOAD_FOLDER)
 
-    # # Processar a pasta extraída com o OCR
-    # process_dataset(extract_folder)
+    # Processar a pasta extraída com o OCR
+    process_dataset(extract_folder)
 
 
-    # # Definir paths para as pastas de textos formatados e resumidos
-    # formatted_folder = os.path.join(extract_folder, "Textos-Formatados")
+    # Definir paths para as pastas de textos formatados e resumidos
+    formatted_folder = os.path.join(extract_folder, "Textos-Formatados")
     summarized_folder = os.path.join(extract_folder, "Textos-Resumidos")
     os.makedirs(summarized_folder, exist_ok=True)
 
-    # # Processar os textos formatados e gerar os resumos individuais
-    # process_summary_individual(formatted_folder, summarized_folder)
+    final_summary_path = os.path.join(summarized_folder, "resumo_final.txt")
+  
+    # Verificar se o resumo final já existe
+    if os.path.exists(final_summary_path):
+        # Ler o conteúdo do resumo final existente
+        with open(final_summary_path, "r", encoding="utf-8") as f:
+            final_summary_content = f.read()
+
+        return {
+            "detail": "Resumo final resgatado.",
+            "final_summary": final_summary_content
+        }
+
+    # Processar os textos formatados e gerar os resumos individuais
+    process_summary_individual(formatted_folder, summarized_folder)
     
     # Processar os resumos individuais e gerar o resumo final
-    final_summary_path = os.path.join(summarized_folder, "resumo_final.txt")
-    #process_summary_final(summarized_folder, final_summary_path)
+    process_summary_final(summarized_folder, final_summary_path)
     
     # Conteúdo do resumo final
     with open(final_summary_path, "r", encoding="utf-8") as f:
@@ -50,6 +62,6 @@ async def generate_summary(file: UploadFile = File(...)):
 
     # Retornar o resumo final em formato JSON
     return {
-        "detail": "Upload e processamento concluídos com sucesso!",
+        "detail": "Resumo final gerado com sucesso.",
         "final_summary": final_summary_content
     }
--- src/frontend/js/index.js
+++ src/frontend/js/index.js
@@ -1,23 +1,16 @@
 document.getElementById("attach-file").onchange = function (event) {
   const fileName = event.target.files[0].name;
-  const file = event.target.files[0];
 
   if (!fileName.endsWith(".zip")) {
     alert("O arquivo deve ser no formato ZIP.");
     return;
   }
-
-  //const pdfIcon = document.createElement("img");
-  //pdfIcon.src = "assets/img/zip-icon.svg"; // Altere para um ícone de ZIP
-  //pdfIcon.alt = "Imagem de um arquivo ZIP";
-
-  // var image = document.getElementById("icon-file-attach");
-  //image.setAttribute("src", "assets/img/zip-icon.svg");
   document.getElementById("label-file-attach").innerHTML = fileName;
 };
 
 let base64 = null;
 
+// Ação de extração do arquivo
 document.getElementById("extract").onclick = function (event) {
   const fileInput = document.getElementById("attach-file");
   const file = fileInput.files[0];
@@ -27,14 +20,18 @@ document.getElementById("extract").onclick = function (event) {
     return;
   }
 
+  // Limpa o resumo anterior
+  const resultadoElement = document.getElementById("message-txt");
+  resultadoElement.innerHTML = "";
+
   const formData = new FormData();
-  formData.append("file", file); // Adiciona o arquivo ZIP ao FormData
+  formData.append("file", file);
 
   document.getElementById("chat-loader").style.display = "flex";
   window.scrollTo(0, 500);
 
+  // Fetch no backend para processar o arquivo
   fetch("http://127.0.0.1:8000/generate_summary/", {
-    // Adicionado "http://"
     method: "POST",
     body: formData,
   })
@@ -62,306 +59,3 @@ document.getElementById("extract").onclick = function (event) {
       console.error("Erro ao enviar o arquivo:", error);
     });
 };
-
-document.getElementById("ask-assistent-submit").onclick = function (event) {
-  const input = document.getElementById("ask-assistent-input");
-  const ask = input.value;
-
-  if (input.value.length === 0) {
-  } else {
-    newMessage({
-      messageText: input,
-      isAssistent: false,
-    });
-    const uuidSalvo = localStorage.getItem("UUID");
-
-    fazerRequisicao_rag(ask, uuidSalvo)
-      .then((body) => {
-        console.log("requisição");
-        console.log(uuidSalvo);
-        console.log(body);
-        verificarEndpoint(body)
-          .then((body) => {
-            console.log("Body da requisição bem-sucedida:", body);
-            newMessage({
-              messageText: body,
-            });
-          })
-          .catch((error) => {
-            console.error("Erro ao verificar a requisição:", error);
-            // Trate os erros aqui, se necessário
-          });
-      })
-      .catch((error) => {
-        console.error("Erro na obtenção do body:", error);
-        // Trate os erros aqui, se necessário
-      });
-  }
-};
-
-function newMessage({ messageText, isAssistent = true }) {
-  var newMessageBox = document.createElement("div");
-  newMessageBox.classList.add("message-box");
-  var messageSenderWrapper = document.createElement("div");
-
-  messageSenderWrapper.id = "sender";
-
-  var sender = document.createElement("span");
-  sender.id = "identifier";
-  sender.innerText = isAssistent ? "Assistente STF" : "Você";
-  messageSenderWrapper.appendChild(sender);
-
-  if (isAssistent) {
-    var copyUtil = document.createElement("span");
-    copyUtil.innerText = "Copiar";
-    var idNumber = Math.random();
-    copyUtil.id = "copy" + idNumber;
-    copyUtil.style = "cursor: pointer;";
-    copyUtil.onclick = () => {
-      var idToCopy = document.getElementById(copyUtil.id).parentElement
-        .nextSibling.id;
-      console.log(idToCopy);
-      CopyToClipboard(idToCopy);
-      copyUtil.innerHTML = "<i>Copiado</i>";
-      setTimeout(() => {
-        copyUtil.innerText = "Copiar";
-      }, 1000);
-    };
-    messageSenderWrapper.appendChild(copyUtil);
-  }
-
-  var message = document.createElement("div");
-  message.classList.add("message");
-  isAssistent ? (message.id = "message" + idNumber) : "";
-  message.innerText = isAssistent ? messageText : messageText.value;
-
-  newMessageBox.appendChild(messageSenderWrapper);
-  newMessageBox.appendChild(message);
-
-  document.getElementById("chat").appendChild(newMessageBox);
-
-  !isAssistent ? (messageText.value = "") : () => {};
-}
-
-function CopyToClipboard(containerid) {
-  var range = document.createRange();
-  range.selectNode(document.getElementById(containerid));
-  window.getSelection().removeAllRanges(); // clear current selection
-  window.getSelection().addRange(range); // to select text
-  document.execCommand("copy");
-  window.getSelection().removeAllRanges();
-}
-
-//minhas funcs
-
-function fazerRequisicao_rag(ask, uuid) {
-  const url = "COLOQUE AQUI O LINK ";
-
-  const payload = {
-    prompt: ask,
-    uuid: uuid,
-  };
-
-  const headers = {
-    Authorization: "",
-    "Content-Type": "application/json",
-  };
-
-  const requestOptions = {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(payload),
-  };
-
-  return fetch(url, requestOptions)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      return data.body; // Retornar o UUID obtido da resposta
-      // Faça o que quiser com a resposta aqui
-    })
-    .catch((error) => {
-      console.error("Erro:", error);
-      // Trate os erros aqui
-    });
-}
-
-function separarPerguntas(textoBruto) {
-  // Separação do texto por cada pergunta
-  let perguntas = textoBruto.match(/[A-Z]\).*?(?=[A-Z]\)|$)/gs);
-
-  // Formatando as perguntas uma abaixo da outra
-  let perguntasFormatadas = perguntas.map((pergunta) => {
-    return `<p>${pergunta}</p>\n`;
-  });
-
-  return perguntasFormatadas.join("");
-}
-
-function fazerRequisicao_doc(base64, id) {
-  const url = "";
-
-  const payload = {
-    base64: base64,
-    id: id,
-  };
-
-  const headers = {
-    Authorization: "",
-    "Content-Type": "application/json",
-  };
-
-  const requestOptions = {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(payload),
-  };
-
-  return fetch(url, requestOptions)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      return data.body.uuid; // Retornar o UUID obtido da resposta
-      // Faça o que quiser com a resposta aqui
-    })
-    .catch((error) => {
-      console.error("Erro:", error);
-      // Trate os erros aqui
-    });
-}
-
-function fazerRequisicao_texto(numeroProcesso, id) {
-  const url = "";
-
-  const payload = {
-    doc: numeroProcesso,
-    id: id,
-  };
-
-  const headers = {
-    Authorization: "",
-    "Content-Type": "application/json",
-  };
-
-  const requestOptions = {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(payload),
-  };
-
-  return fetch(url, requestOptions)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      return data.body.uuid; // Retornar o UUID obtido da resposta
-      // Faça o que quiser com a resposta aqui
-    })
-    .catch((error) => {
-      console.error("Erro:", error);
-      // Trate os erros aqui
-    });
-}
-
-async function verificarEndpoint(uuid) {
-  return new Promise((resolve, reject) => {
-    const url = "";
-
-    const payload = {
-      uuid: uuid,
-    };
-
-    const headers = {
-      Authorization: "",
-      "Content-Type": "application/json",
-    };
-
-    const requestOptions = {
-      method: "POST",
-      headers: headers,
-      body: JSON.stringify(payload),
-    };
-
-    const verificar = async () => {
-      try {
-        const response = await fetch(url, requestOptions);
-        const data = await response.json();
-        const status = data.statusCode;
-
-        if (status === 200) {
-          console.log("Requisição bem-sucedida. Status:", status);
-          console.log(data);
-          resolve(data.body); // Resolvendo a Promessa com data.body
-        } else {
-          console.log("Requisição ainda não foi bem-sucedida. Status:", status);
-          console.log(data);
-          // Se necessário, aguarde e faça uma nova chamada
-          setTimeout(verificar, 3000); // Chama a função novamente após 3 segundos (ou outro intervalo desejado)
-        }
-      } catch (error) {
-        console.error("Erro na requisição:", error);
-        reject(error); // Rejeita a Promessa em caso de erro
-      }
-    };
-
-    verificar(); // Inicia a verificação
-  });
-}
-
-function lerArquivo(file) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-
-    reader.onload = function (event) {
-      const base64Data = event.target.result.split(",")[1];
-      resolve(base64Data); // Resolvendo a Promise com base64Data
-    };
-
-    reader.onerror = function (error) {
-      reject(error); // Rejeitando a Promise em caso de erro
-    };
-
-    reader.readAsDataURL(file);
-  });
-}
-
-document
-  .getElementById("ask-assistent-form")
-  .addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-      console.log("teste");
-      e.preventDefault();
-    }
-  });
-
-document
-  .getElementById("numero-processo")
-  .addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-      console.log("teste");
-      e.preventDefault();
-    }
-  });
-
-function formatTextWithHTML(text) {
-  const questionsArray = text.split(/(?=[A-Z]\))/).filter(Boolean);
-  const formattedQuestions = questionsArray.map((question) => {
-    const trimmedQuestion = question.trim();
-    return `<p>${trimmedQuestion}</p>`;
-  });
-  console.log(formattedQuestions.join("\n"));
-  return formattedQuestions.join("\n");
-}
-
-function addBreakBeforePhrase(text) {
-  const phrase =
-    "As perguntas abaixo não cabem a este documento, portanto não são informadas:";
-  const index = text.indexOf(phrase);
-
-  if (index !== -1) {
-    const beforePhrase = text.substring(0, index);
-    const afterPhrase = text.substring(index);
-    return `${beforePhrase}<br/>${afterPhrase}`;
-  }
-
-  return text;
-}
